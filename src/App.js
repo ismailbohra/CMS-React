8b829034ee89216634293c5ca1c7681357ed2aee
ismail bohra <ismailbohra99@gmail.com>
2022-11-22 14:49:44 +0530

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Component } from 'react';
 import './App.css';
 import Greet from './component/greeting';
+import Welcome from './component/welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <Greet></Greet>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet></Greet>
+        <Welcome />            {/*self closing tag can be written in single name ending with forward slash*/}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/greeting.js
+++ src/component/greeting.js
@@ -1,6 +1,15 @@
 import React from 'react'
+/*
+old vesrion
 function Greet(){
-    return <h1>Hello Ismail bohra</h1>
+    return <h1>Welcome to CMS</h1>
 }
 
+*/
+const Greet=()=>{
+    return <h1>Welcome to CMS</h1>
+}
+
+
 export default Greet
+
