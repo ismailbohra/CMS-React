a5668c11528c78f22063049dd0d670400b584a9d
ismail bohra <ismailbohra99@gmail.com>
2022-11-22 15:01:20 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import Greet from './component/greeting';
+import Hello from './component/jsx';
 import Welcome from './component/welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet></Greet>
-        <Welcome />            {/*self closing tag can be written in single name ending with forward slash*/}
+        <Hello/>
       </div>
     );
   }
---
+++ src/component/jsx.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello=()=>{
+    return(
+        <div className="tutorial">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Hello
