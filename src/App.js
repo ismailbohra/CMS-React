05d3c8c203ed05b5274e08a9dec204b976c93727
ismail bohra <ismailbohra99@gmail.com>
2022-11-22 14:23:57 +0530

    lesson 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greet from './component/greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet></Greet>
+    </div>
+  );
+}
+
+export default App;
